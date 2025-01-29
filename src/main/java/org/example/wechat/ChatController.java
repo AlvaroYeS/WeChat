16a8e1b8810f9f3af14ca1ac588394538ca74a70
AlvaroYS <1808.ays@gmail.com>
2025-01-29 15:26:52 +0000

    .

--- src/main/java/org/example/wechat/ChatController.java
+++ src/main/java/org/example/wechat/ChatController.java
@@ -18,22 +18,36 @@ import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ChatController implements Initializable {
 
+
     @FXML
     private GridPane chat;
 
+    public static String id;
+    public static String id2;
+    public static String idUser;
+    public ArrayList<Mensaje> listaMensajes;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         chat.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
         chat.setHgap(10);
         chat.setVgap(10);
         chat.setPadding(new Insets(10, 10, 10, 10));
         for (int i = 0; i < Conexion.ListaMensajes.size(); i++) {
+
+            if (!id.equals(Conexion.ListaMensajes.get(i).getId_chat())){
+                if (!id2.equals(Conexion.ListaMensajes.get(i).getId_chat())) {
+                    break;
+                }
+            }
             String estado = "";
 
             if (Conexion.ListaMensajes.get(i).getEstado().equals(Estado.no_entregado)) {
@@ -46,7 +60,7 @@ public class ChatController implements Initializable {
 
             Bubble bubble = new Bubble(Conexion.ListaMensajes.get(i).getMensaje(), (Conexion.ListaMensajes.get(i).getFecha_envioDate().format(DateTimeFormatter.ofPattern("hh:mm")) + " " + estado));
 
-            if (Objects.equals(Conexion.ListaMensajes.get(i).getId_emisor(), "123456789")) {
+            if (Objects.equals(Conexion.ListaMensajes.get(i).getId_emisor(), idUser)) {
                 GridPane.setHalignment(bubble, HPos.RIGHT);
             } else {
                 bubble = new Bubble(Conexion.ListaMensajes.get(i).getMensaje(),Conexion.ListaMensajes.get(i).getFecha_envioDate().format(DateTimeFormatter.ofPattern("hh:mm")));
